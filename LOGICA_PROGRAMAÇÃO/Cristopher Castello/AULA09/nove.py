dc0433b28b01655ae351435dcf659ad5e0b10348
Cristopher-Costa <ffire97@outlook.com>
2026-05-13 11:26:14 -0300

    atualização

--- LOGICA_PROGRAMAÇÃO/Cristopher Castello/AULA09/nove.py
+++ LOGICA_PROGRAMAÇÃO/Cristopher Castello/AULA09/nove.py
@@ -1,1 +1,81 @@
-print("Bem-Vindo ao GitHub")
+# 1. O Problema da Idade
+# idade = input("Digite sua idade: ")
+# if idade >= 18:
+# print("Você é maior de idade.")
+
+# Corrigido
+# idade = int(input("Digite sua idade: "))
+# if idade >= 18:
+#     print("Você é maior de idade.")
+# else:
+#     print("Você não é maior de idade")
+
+# 2. A Escrita Fiel
+# nome = "Mariana"
+# print("Seja bem-vinda, nome!")
+
+# Corrigido
+# nome = input("Digite seu nome: ")
+# print(f"Seja bem-vinda, {nome}")
+
+# 3. Falta de Espaço
+# numero = 10
+# if numero > 5:
+# print("O número é maior que cinco.")
+# else:
+# print("O número é menor ou igual a cinco.")
+
+# Corrigido
+# numero = 10
+# if numero > 5:
+#     print("O número é maior que cinco.")
+# else:
+#     print("O número é menor ou igual a cinco.")
+
+# 4. Esquecimento Fatal
+# usuario = "aluno123"
+# if usuario == "aluno123"
+# print("Login realizado com sucesso.")
+
+# Corrigido
+# usuario = "aluno123"
+# if usuario == "aluno123":
+#     print("Login realizado com sucesso.")
+
+# 5. Atribuição vs. Comparação
+# clima = "ensolarado"
+# if clima = "chuvoso":
+# print("Leve um guarda-chuva!")
+
+# Corrigido
+# clima = "ensolarado"
+# if clima == "chuvoso":
+#     print("Leve um guarda-chuva!")
+
+# 6. Misturando Alhos com Bugalhos
+# pontos = 50
+# print("Parabéns! Você fez " + pontos + " pontos.")
+
+# Corrigido
+# alhos = "pontos"
+# bugalhos = "pontos"
+# pontos = 50
+# print("Parabéns! Você fez" +  pontos   +  "pontos")
+
+# 7. A Ordem dos Fatores
+# O sistema deve dar "Excelente" para notas 9 ou 10.
+# nota = 9.5
+# if nota >= 7:
+# print("Aprovado")
+# elif nota >= 9:
+# print("Excelente!")
+
+# Corrigido
+input("Digite sua nota: ")
+nota = 9.5
+if nota <= 7:
+    print("Aprovado")
+elif nota >= 9:
+    print("Excelente!")
+else:
+    print("Parabens!!")

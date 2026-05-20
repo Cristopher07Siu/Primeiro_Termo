02ed67005b7cbc542cda55ec4bf9ce87b17133fb
Cristopher-Costa <ffire97@outlook.com>
2026-05-20 08:52:45 -0300

    aulaa

--- LOGICA_PROGRAMAÇÃO/Cristopher Castello/AULA09/nove.py
+++ LOGICA_PROGRAMAÇÃO/Cristopher Castello/AULA09/nove.py
@@ -10,6 +10,23 @@
 # else:
 #     print("Você não é maior de idade")
 
+# Melhorado
+# while True:
+#     entrada = input("Digite sua idade (ou 'sair' para encerrar): ")
+    
+#     if "sair":
+#         print("Encerrando o programa...")
+#         break
+    
+#     try:
+#         idade = int(entrada)
+#         if idade >= 18:
+#             print("Você é maior de idade.")
+#         else:
+#             print("Você não é maior de idade.")
+#     except ValueError:
+#         print("Por favor, digite um número válido.")
+
 # 2. A Escrita Fiel
 # nome = "Mariana"
 # print("Seja bem-vinda, nome!")
@@ -17,6 +34,7 @@
 # Corrigido
 # nome = input("Digite seu nome: ")
 # print(f"Seja bem-vinda, {nome}")
+
 
 # 3. Falta de Espaço
 # numero = 10
@@ -71,11 +89,53 @@
 # print("Excelente!")
 
 # Corrigido
-input("Digite sua nota: ")
-nota = 9.5
-if nota <= 7:
-    print("Aprovado")
-elif nota >= 9:
-    print("Excelente!")
-else:
-    print("Parabens!!")
+# input("Digite sua nota: ")
+# nota = 9.5
+# if nota <= 7:
+#     print("Aprovado")
+# elif nota >= 9:
+#     print("Excelente!")
+# else:
+#     print("Parabens!!")
+
+# 8. O Contador de 1 a 5
+# Objetivo: Mostrar na tela os números 1, 2, 3, 4 e 5.
+# for i in range(5):
+# print(i)
+
+# Corrigido
+# for contador in range (1, 6):
+#     print(f"{contador}")
+
+# 9. O Loop Eterno
+# tentativas = 1
+# while tentativas <= 3:
+# print("Tentando conectar...")
+# O código deveria parar após 3 tentativas
+
+# Corrigido
+# tentativas = 1
+# while tentativas <= 3:
+#     print("Tentando conectar...")
+#     tentativas += 1
+# print("Fim das tentativas.")
+
+# 10. A Senha Teimosa
+# O programa deve pedir a senha até que o usuário digite "python123"
+# senha = ""
+# while senha == "python123":
+# senha = input("Digite a senha secreta: ")
+# print("Acesso concedido!")
+
+# Corrigido
+# senha = input("Digite sua senha secreta: ")
+# while senha == "python123":
+#     print("Acesso concedido")
+#     break
+
+# Melhorado
+# senha = input("Digite sua senha secreta: ")
+# while senha == "python123" or senha == "Python123":
+#      print("Acesso concedido")
+#      break
+# print("Sistema finalizado.")
